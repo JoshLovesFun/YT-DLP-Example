0a177c3c916c2ac8a6a6bdfe9dd7f5a1d52b9b1b
JoshLovesFun <ihtftl@gmail.com>
2024-08-30 14:20:13 -0400

    small changes

--- YT_DLP_Example.py
+++ YT_DLP_Example.py
@@ -3,16 +3,19 @@ import yt_dlp
 print('Hello Josh!')
 print('')
 
-video_urls = [  # 'https://youtu.be/_o-XIryB2gg?si=KugFTg0Rl2EEYJYA',
+video_urls = [
+    # 'https://youtu.be/_o-XIryB2gg?si=KugFTg0Rl2EEYJYA',
     # 'https://youtu.be/ca48oMV59LU?si=AmHT5onKmFq0N7ZW',
     # 'https://youtu.be/y26J3T3WT0I?si=XFX70dhYu0jVE6LZ',
     # 'https://youtu.be/gMQv5i3wQeQ?si=HhCtKmUnhfc3MDU2',
     'https://youtu.be/0bmE9XY3sOc?si=fzVi7YrDT6SRKSwP',
-    'https://youtube.com/shorts/l_U9MOWVmpo?si=aYrmA5tnugEIlr03']
+    'https://youtube.com/shorts/l_U9MOWVmpo?si=aYrmA5tnugEIlr03',
+]
 
-# This can be used on phone as well with Pydroid 3. Just use line below as example.
+# This can be used on phone as well with Pydroid 3.
+# Just use line below as example.
 # output_dir = '/storage/emulated/0/Documents/MusicFromYT_Phone/AE'
-output_dir = 'C:\\PyTest'
+output_dir = 'C:\\Code\\Python\\GitHub\\YT-DLP-Example'
 
 # Audio
 ydl_opts_audio = {
@@ -37,9 +40,6 @@ with yt_dlp.YoutubeDL(ydl_opts_video) as ydl_v:
         ydl_v.download([video_url])
 
 print('')
-
 print('Nice!')
-
 print('')
-
 print('That is amazing!!!')
